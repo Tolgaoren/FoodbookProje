d110fc5541e72dbe514b14aa1f5f2b12b0aed957
orent <orentolga@hotmail.com>
2022-12-14 21:23:50 +0300

    final

--- app/src/main/java/com/toren/foodbookapp/ui/view/FoodFragment.kt
+++ app/src/main/java/com/toren/foodbookapp/ui/view/FoodFragment.kt
@@ -13,8 +13,10 @@ import coil.load
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.toren.foodbookapp.R
+import com.toren.foodbookapp.adapter.CommentsAdapter
 import com.toren.foodbookapp.adapter.IngredientsAdapter
 import com.toren.foodbookapp.databinding.FoodFragmentBinding
+import com.toren.foodbookapp.model.Users
 import com.toren.foodbookapp.model.Yemek
 import com.toren.foodbookapp.ui.viewmodel.FoodViewModel
 
@@ -24,9 +26,11 @@ class FoodFragment : Fragment() {
     private var _binding: FoodFragmentBinding? = null
     private val binding get() = _binding!!
     private lateinit var materialAdapter : IngredientsAdapter
+    private lateinit var commentsAdapter: CommentsAdapter
     private val args: FoodFragmentArgs by navArgs()
     private val auth = Firebase.auth
     private lateinit var recipe : Yemek
+    private lateinit var name : Users
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -43,8 +47,22 @@ class FoodFragment : Fragment() {
         viewModel.loadRecipe(args.gelenYemek)
         loadRecipe()
 
+        viewModel.thisUser()
+        loadUser()
+
         binding.apply {
 
+            commentPush.setOnClickListener {
+                if (foodComment.text.isNotBlank() && foodComment.text.isNotEmpty()) {
+
+                    val id = args.gelenYemek.yemekIsmi + "?user=" + args.gelenYemek.user
+                    val comment = name?.name + " "+ name?.surname + ": " + foodComment.text.toString()
+
+                    viewModel.comment(comment, id)
+
+                    foodComment.text.clear()
+                }
+            }
 
             likeButton.setOnCheckedChangeListener { _, isChecked ->
                 if (isChecked) {
@@ -58,7 +76,14 @@ class FoodFragment : Fragment() {
                 }
             }
         }
+    }
 
+    private fun loadUser() {
+        viewModel.name.observe(viewLifecycleOwner){
+            it?.let {
+                name = it
+            }
+        }
     }
 
     private fun loadRecipe() {
@@ -90,11 +115,16 @@ class FoodFragment : Fragment() {
             else if(likeCheck.isEmpty()){
                 likeButton.background = context?.getDrawable(R.drawable.ic_favorite_border)
             }
-            materialAdapter = IngredientsAdapter(recipe.malzemeler)
-
+            materialAdapter = IngredientsAdapter(recipe.malzemelerDetayli)
+            commentsAdapter = CommentsAdapter(recipe.comments)
 
             recyclerViewMalzemeler.layoutManager = LinearLayoutManager(view?.context)
             recyclerViewMalzemeler.adapter = materialAdapter
+
+            recyclerViewYorumlar.layoutManager = LinearLayoutManager(view?.context)
+            recyclerViewYorumlar.adapter = commentsAdapter
+
+
         }
     }
 
